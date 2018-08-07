bd174faae5c42bbaafdd12235cb6c595452cb868
natashabhuta <natasha.bhuta@gmail.com>
2018-08-07 12:55:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,11 +3,11 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears GROUP BY ASC;"
+  "SELECT name FROM bears ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "S"
+  "SELECT name, age FROM bears Order "
 end
 
 def selects_oldest_bear_and_returns_name_and_age
